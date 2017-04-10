4c86a7511fdbbd14d8057d5b9e53c7186c5523db
huanlu <1466022881@qq.com>
2017-04-10 13:58:48 +0800

    修改program5

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -1,37 +1,89 @@
 #include<iostream>
 using namespace std;
 
-class Matrix {   
-	friend Matrix operator+(const Matrix &a, const Matrix &b); 
-	friend Matrix operator*(const Matrix &a, const Matrix &b); 
-public :   
-	Matrix(int l,int c);
-	Matrix(const Matrix &m);
-	~Matrix();
-	void setLine(int l);  
-	void setCol(int c);  
-	void setElems();
+const int M=2,N=3;
+class Matrix
+{
+private:
+	int a[M][N];//M行 N列 数组； 
+public:
+	Matrix()
+	{
+		for(int i=0;i<M;i++)
+			for(int j=0;j<N;j++)
+				a[i][j]=0;
+	}
+	
+	Matrix(const Matrix& ma)
+	{
+		for(int i=0;i<M;i++)
+			for(int j=0;j<N;j++)
+				a[i][j]=ma.a[i][j];
+				
+		cout<<"<拷贝构造函数>拷贝矩阵完成~ "<<endl;
+	}
+	
+	Matrix operator+ (Matrix& m)
+	{
+		Matrix c;
+		for(int i=0;i<M;i++)
+			for(int j=0;j<N;j++)
+					c.a[i][j]=a[i][j]+m.a[i][j];
+				
+		return c;
+		
+	}
 
-	int getLine() const;  
-	int getCol() const;  
-	void print() const;     
-	Matrix& operator= (const Matrix &m);  
-	Matrix operator~ () const;   
-private :  
-	int line;  
-	int col;  
-	int *elems; 
+	//公式忘了，假设为 对应位置上的数值相乘 
+	Matrix operator* (Matrix& ma)
+	{
+		Matrix c;
+		for(int i=0;i<M;i++)
+			for(int j=0;j<N;j++)
+					c.a[i][j]=a[i][j] * ma.a[i][j];
+
+		return c;
+	}
+
+
+	friend ostream& operator<< (ostream &output ,Matrix &m)
+	{
+		for(int i=0;i<M;i++)
+		{
+			for(int j=0;j<N;j++)
+				output<<m.a[i][j]<<"    ";
+
+			output<<endl;
+		}
+
+		return output;
+	}
+
+	friend istream& operator>> (istream &input,Matrix &m)
+	{
+			for(int i=0;i<M;i++)
+				for(int j=0;j<N;j++)
+					input>>m.a[i][j];
+			return input;
+	}
 };
 
-Matrix::Matrix(int l,int c) {  
-	setLine(l); 
-	setCol(c);  
-	elems = new int[col*line]; 
-}  
+		
+int main()
+		{
+	        Matrix m1,m3;
+			cout<<"请输入一个"<<M<<"行"<<N<<"列矩阵的各值: "<<endl;
+			cin>>m1;
+			
+			Matrix m2(m1);
+			
+			m3=m1+m2;//这个编译环境重载有问题，只能先计算 赋值，再输出。 
+			cout<<"两个矩阵的和为："<<endl;
+			cout<<m3;
 
-Matrix::Matrix(const Matrix &m) {  
-	line = m.line; col = m.col;  
-	elems = new int[line * col]; 
-	for(int i = 0;i < line * col;i++) 
-		elems[i]= m.elems[i]; 
-} 
+			m3=m1*m2;
+			cout<<"两个矩阵的积为："<<endl;
+			cout<<m3;
+			
+			return 0;
+}
